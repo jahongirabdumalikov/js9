115b14146329af4ec7264bbf67bc4c9a6012087e
jahongirabdumalikov <abdumalikovj015@gmail.com>
2024-09-26 09:33:40 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// function invertArray(arr) {
+//     return [...arr].reverse(); 
+// }
+// const originalArray = [1, 2, 3];
+// const invertedArray = invertArray(originalArray);
+
+// console.log(invertedArray); 
+// console.log(originalArray); 
+
+
+// //////////  2  //////////
+
+// function getBonus(baseAmount, isBonus) {
+//     return isBonus ? baseAmount * 10 : baseAmount;
+//   }
+//   console.log(getBonus(500, true));
+//   console.log(getBonus(100, false));
+  
+
+//   ////////  3  ///////////
+
+// function solution(str) {
+//     return str.split("").reverse().join("");
+//   }
+  
+//   console.log(solution("drow"));
+//   console.log(solution("something"));
+
+//   //////////  4  //////////
+
+
+// function gym(i, u, k) {
+//     return Math.max(u + k - i, 0);
+//   }
+//   console.log(gym(10, 5, 5));
+
+
+//   //////  5  ///////////
+
+// function formatMoney(amount) {~
+//     return `$${amount.toFixed(2)}`;
+//   }
+//   console.log(formatMoney(39.99));
+//   console.log(formatMoney(3));
+//   console.log(formatMoney(9.1));
